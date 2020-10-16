5b4eb938dbceae50c11b93c06149f67c497f60dc
look-dj <look-dj@outlook.com>
2020-10-16 20:41:01 +0800

    .

--- app.js
+++ app.js
@@ -1,31 +1,36 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var multer = require('multer');
-var app = express();
-var ejs = require('ejs');
-var bodyParser = require('body-parser')
-let router = require('./routes/index.js'); // 路由
-let config = require('./config/config.js'); // 配置
-let plugins = require('./plugins/index.js');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const multer = require("multer");
+const app = express();
+const ejs = require("ejs");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+let router = require("./routes/index.js"); // 路由
+let config = require("./config/config.js"); // 配置
+let plugins = require("./plugins/index.js");
 config(app);
 plugins(app);
-app.set('views', path.join(__dirname, 'views'));
+app.set("views", path.join(__dirname, "views"));
 //设置模板引擎为ejs
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 //为html扩展名注册ejs
-app.engine('html', ejs.renderFile);
+app.engine("html", ejs.renderFile);
 let obj = multer({
-	dest: app.config.savePath
+	dest: app.config.savePath,
 });
+app.use(cors());
 app.use(obj.any());
 app.use(express.json());
-app.use(express.urlencoded({
-	extended: false
-}));
-app.use(bodyParser.json())
+app.use(
+	express.urlencoded({
+		extended: false,
+	})
+);
+app.use(bodyParser.json());
 app.use(cookieParser());
-app.use("/static", express.static(path.join(__dirname, 'static')));
-app.use("/public/upload", express.static(path.join(__dirname, 'upload')));
+app.use("/static", express.static(path.join(__dirname, "static")));
+app.use("/public/upload", express.static(path.join(__dirname, "upload")));
 router(app);
 module.exports = app;
